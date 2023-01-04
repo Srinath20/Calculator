8cb839ba5b6264400c79f23941b0b413e3075a54
Srinath20 <srinathgollapinni@gmail.com>
2023-01-04 15:16:05 +0530

    unnecessary lines taken out

--- script.js
+++ script.js
@@ -1,35 +1,10 @@
-var operater;
 var result = document.getElementById("result");
-//let numbers = document.querySelectorAll('.number');
-// numbers.forEach(number => number.addEventListener("click",input));
 let buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener("click",input));
 function input(e){
     const value = e.target.value;
     result.textContent +=value;
-    console.log(value);
   }
-function add(){
- // operater = document.getElementById("+").value;
-  document.getElementById("result").value += "+";
-  document.getElementById("result").textContent += "+";
-}
-function subtract(){
- // operater = document.getElementById("-").value;
-  document.getElementById("result").value += "-";
-  document.getElementById("result").textContent += "-";
-}
-function multiply(){
- // operater = document.getElementById("*").value;
-  document.getElementById("result").value += "*";
-  document.getElementById("result").textContent += "*";
-}
-function divide(){
-//  operater = document.getElementById("/").value;
-  document.getElementById("result").value += "/";
-  document.getElementById("result").textContent += "/";
-}
-
 function operate(){ 
 
   const ans = eval(result.textContent);
